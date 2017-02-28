a9883865b89f0f4cdad487f132973399ab567c09
david <david.gnext@gmail.com>
2017-02-28 16:22:19 +0630

    fragment

--- app/src/main/java/com/example/david/tutorial/G1.java
+++ app/src/main/java/com/example/david/tutorial/G1.java
@@ -8,9 +8,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class G1 extends Fragment {
+    TextView quote;
+
+    public static final String QOUTE_ID = "quoteID";
+
+    public G1() {
+
+    }
+
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle saved) {
-        return inflater.inflate(R.layout.lm_fragment,container,false);
+
+        View view = inflater.inflate(R.layout.lm_fragment_1,container,false);
+        quote = (TextView) view.findViewById(R.id.bible_quote);
+        quote.setText(getArguments().getString(QOUTE_ID));
+        return view;
+
     }
 }
